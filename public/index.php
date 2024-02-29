e2a683bf4948f09ce9bb9e52a72b7be4cbbd4e0a
thtocha <max.tatochenko@gmail.com>
2024-02-29 09:42:58 +0200

    Change position items in table, replace button edit on icon. view demo complete

--- public/index.php
+++ public/index.php
@@ -53,7 +53,7 @@
                                 <td><input type="checkbox"></td>
                                 <td><?= $user['first_name'] . ' ' . $user['last_name']?></td>
                                 <td><?= $user['role'] ?></td>
-                                <td>
+                                <td style="vertical-align: middle; text-align: center">
                                     <div class="d-flex justify-content-center">
                                         <?php if ($user['status'] == 1): ?>
                                             <div style="width: 20px; height: 20px; border-radius: 50%; background-color: green;"></div>
@@ -65,7 +65,11 @@
                                 <td>
                                     <div class="d-flex justify-content-center">
                                         <div class="btn-group text-center">
-                                            <button type="button" class="btn btn-outline-dark" style="font-size: 0.7rem" data-toggle="modal" data-target="#updateUserModal">Edit</button>
+                                            <button type="button" class="btn btn-outline-dark" style="font-size: 0.7rem" data-toggle="modal" data-target="#updateUserModal">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+                                                </svg>
+                                            </button>
                                             <button type="button" class="btn btn-outline-dark" style="font-size: 0.6rem" data-toggle="modal" data-target="#deleteUserModal">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
@@ -126,6 +130,7 @@
                         <div class="custom-control custom-switch">
                             <input type="checkbox" class="custom-control-input" id="customSwitch1">
                             <label class="custom-control-label" for="customSwitch1">Status</label>
+<!--                            todo status size and replace-->
                         </div>
                     </div>
                     <div class="mb-3">
