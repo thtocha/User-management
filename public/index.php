e8328f4f5f558f55194677b30f1b89620deaf8eb
thtocha <max.tatochenko@gmail.com>
2024-03-13 21:59:51 +0200

    refactor: change svg to bootstrap icons

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
           integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>User management</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
